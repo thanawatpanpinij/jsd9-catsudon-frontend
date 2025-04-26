ab12f35c557402243e98ad3f70325adba5744f05
KanittaJHA <kanittaketin@gmail.com>
2025-04-26 23:14:40 +0700

    feat: add FreshFlavorful section with image slider and collapsible items

--- src/components/01c-landing-page/FreshFlavorful.jsx
+++ src/components/01c-landing-page/FreshFlavorful.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
 import { RiArrowDownSLine } from "react-icons/ri";
 
 const FreshFlavorful = () => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  const items = [
+    {
+      title: "Healthy Cruciferous",
+      description:
+        "Cruciferous vegetables belong to the Brassicaceae family and include vegetables such as broccoli, cauliflower, cabbage, Brussels sprouts, kale, and others.",
+    },
+    {
+      title: "Fresh Root Vegetables",
+      description:
+        "Root vegetables like carrots, sweet potatoes, and beets are rich in nutrients and fiber, promoting digestion and overall health. They're perfect for clean eating.",
+    },
+    {
+      title: "Dry Fruits & Nuts",
+      description:
+        "Dry fruits and nuts are energy-packed, rich in healthy fats and protein. They aid muscle growth, support heart health, and provide essential vitamins and minerals for overall well-being.",
+    },
+    {
+      title: "Organic Vegetables",
+      description:
+        "Organic vegetables are grown without synthetic chemicals, offering better nutrition and being healthier for both you and the environment.",
+    },
+    {
+      title: "Natural Citrus Fruits",
+      description:
+        "Citrus fruits like oranges, lemons, and limes are rich in vitamin C, antioxidants, and fiber, supporting immune health and digestion.",
+    },
+    {
+      title: "Healthy Leafy Green",
+      description:
+        "Leafy greens are rich in vitamins, minerals, and antioxidants, supporting digestion and immunity. Examples include spinach and kale.",
+    },
+  ];
+
   return (
     <div className="max-w-[1440px] mx-auto px-[32px] mb-[56px]">
       <div className="mb-[32px] flex flex-col leading-tight">
@@ -35,7 +74,7 @@ const FreshFlavorful = () => {
           >
             <SwiperSlide>
               <img
-                className="w-full h-[500px] object-cover rounded-4xl"
+                className="w-full h-[512px] object-cover rounded-4xl"
                 src="https://res.cloudinary.com/dsgtmtcmt/image/upload/v1744720550/001-fresh-flavoful_gpgwlj.jpg"
                 alt=""
               />
@@ -43,7 +82,7 @@ const FreshFlavorful = () => {
 
             <SwiperSlide>
               <img
-                className="w-full h-[500px] object-cover rounded-4xl"
+                className="w-full h-[512px] object-cover rounded-4xl"
                 src="https://res.cloudinary.com/dsgtmtcmt/image/upload/v1744720550/002-fresh-flavoful_pmqe8a.webp"
                 alt=""
               />
@@ -51,7 +90,7 @@ const FreshFlavorful = () => {
 
             <SwiperSlide>
               <img
-                className="w-full h-[500px] object-cover rounded-4xl"
+                className="w-full h-[512px] object-cover rounded-4xl"
                 src="https://res.cloudinary.com/dsgtmtcmt/image/upload/v1744978247/003-leaf-lettuce_q3d1qs.avif"
                 alt=""
               />
@@ -59,27 +98,44 @@ const FreshFlavorful = () => {
 
             <SwiperSlide>
               <img
-                className="w-full h-[500px] object-cover rounded-4xl"
+                className="w-full h-[512px] object-cover rounded-4xl"
                 src="https://res.cloudinary.com/dsgtmtcmt/image/upload/v1744978248/004-cherry-tomatoes_yxzerl.avif"
                 alt=""
               />
             </SwiperSlide>
           </Swiper>
 
-          <div className="bg-secondary w-full rounded-4xl p-6">
-            <div className="bg-white p-5 rounded-3xl flex flex-col gap-2">
-              <div className="flex items-center justify-between text-third">
-                <h3 className="font-semibold ">
-                  Healthy Cruciferous
-                </h3>
-                <RiArrowDownSLine />
+          <div className="bg-secondary w-full rounded-4xl p-6 flex flex-col justify-center gap-4">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className={`bg-white p-5 rounded-3xl flex flex-col transition-all duration-300 ${
+                    openIndex === index ? "gap-2" : "gap-0"
+                }`}
+              >
+                <div
+                  className="flex items-center justify-between text-third cursor-pointer"
+                  onClick={() => handleToggle(index)}
+                >
+                  <h3 className="font-semibold">{item.title}</h3>
+                  <RiArrowDownSLine
+                    className={`bg-gray-300 text-normal-size rounded-full text-white hover:bg-primary transition-all duration-300 transform ${
+                      openIndex === index ? "rotate-180 bg-primary" : "rotate-0"
+                    }`}
+                  />
+                </div>
+
+                <div
+                  className={`grid overflow-hidden transition-all duration-500 ease-in-out ${
+                    openIndex === index ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
+                  }`}
+                >
+                  <div className="text-pretty text-small-size text-gray-600 overflow-hidden">
+                    {item.description}
+                  </div>
+                </div>
               </div>
-              <p className="text-pretty text-small-size text-gray-600">
-                Cruciferous vegetables belong to the Brassicaceae family and
-                include vegetables such as broccoli, cauliflower, cabbage,
-                Brussels sprouts, kale, and others.
-              </p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
