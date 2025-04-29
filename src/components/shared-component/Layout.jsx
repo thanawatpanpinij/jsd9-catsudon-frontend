14805ac09f277cf30b58dc7acd0d28e0794964c5
KanittaJHA <kanittaketin@gmail.com>
2025-04-29 16:13:47 +0700

    modified Layout.jsx

--- src/components/shared-component/Layout.jsx
+++ src/components/shared-component/Layout.jsx
@@ -8,7 +8,8 @@ export default function Layout() {
     return (
         <>
             <Header />
-            <Outlet />
+            {/* <Outlet /> */}
+            <AboutUsPage/>
             <Footer />
         </>
     );
