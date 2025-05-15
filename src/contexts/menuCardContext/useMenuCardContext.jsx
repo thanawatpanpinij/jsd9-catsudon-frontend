47b242d08fee66c581cda40ef2223baf3f70ff87
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-05-15 11:54:32 +0700

    fix(useMenuCardContext): fix import path for vercel

--- src/contexts/menuCardContext/useMenuCardContext.jsx
+++ src/contexts/menuCardContext/useMenuCardContext.jsx
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { MenuCardContext } from "./MenuCardContext";
+import { MenuCardContext } from "./menuCardContext.jsx";
 
 export const useMenuCardContext = () => useContext(MenuCardContext);
