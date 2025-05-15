f33fe5809fc8952dbd371f775877c0ec8729b6fc
KanittaJHA <kanittaketin@gmail.com>
2025-05-15 16:29:19 +0700

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -10,21 +10,6 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem("accessToken");
-
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
 axiosInstance.interceptors.request.use(
   (config) => {
     return config;
